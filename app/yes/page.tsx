466dcfba4857f17d3173e8ddec0b37f8faefc6d4
Sviatoslav <nhlsviatoslav@gmail.com>
2024-02-19 10:39:31 +0100

    changed yes page

--- app/yes/page.tsx
+++ app/yes/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div className="w-full h-full flex flex-col items-center">
-      <div>
-        <Image src={"/dancing-heart.gif"} alt='' width={336.5} height={175}/>
-      </div>
+      <Image src={"/dancing-heart.gif"} alt='' layout="responsive" width={624} height={624}/>
       <div>
         <h1 className="font-bold text-3xl text-transparent gradient-text">Yay let's go!</h1>
       </div>
