ecd503e401db3c71480de3b1a8681868b7b3afe9
Sviatoslav <nhlsviatoslav@gmail.com>
2024-02-19 10:48:43 +0100

    fixed layout shift

--- app/yes/page.tsx
+++ app/yes/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="w-full h-full flex flex-col items-center">
-      <Image src={"/dancing-heart.gif"} alt='' layout="responsive" width={624} height={624}/>
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      {/*<div className="h-64 w-64">*/}
+        <Image className="h-64 w-64" src={"/dancing-heart.gif"} alt='' width={624} height={624}/>
+      {/*</div>*/}
       <div>
         <h1 className="font-bold text-3xl text-transparent gradient-text">Yay let's go!</h1>
       </div>
